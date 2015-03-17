d14cfc706ac4ec54b562064c4d6339c6e30062f3
Minich Ivan <minich.job@gmail.com>
2015-03-17 15:38:13 +0200

    and again

--- packages/mailmanager/server/controllers/mailmanager.js
+++ packages/mailmanager/server/controllers/mailmanager.js
@@ -1,14 +1,14 @@
 'use strict';
 
 var mongoose = require('mongoose'),
-    MailBox = mongoose.model('MailBox'),
+    mailBox = mongoose.model('MailBox'),
     PackConfig = mongoose.model('PackConfig'),
     User = mongoose.model('User'),
     request = require('request'),
     _ = require('lodash');
 var config = '';
 
-function sortMailBoxes(arr) {
+function sortMailboxes(arr) {
     var result = _.chain(arr)
         .groupBy('domain')
         .pairs()
@@ -59,7 +59,7 @@ exports.getConfig = function(req, res) {
         });
 };
 exports.getAccessibleMails = function(req, res) {
-    MailBox
+    mailBox
         .find({
             accessedFor: req.user._id,
             state: 1,
@@ -73,50 +73,44 @@ exports.getAccessibleMails = function(req, res) {
                 console.log(err);
                 return res.status(500).send(err);
 
-            } else return res.jsonp(sortMailBoxes(mails));
+            } else return res.jsonp(sortMailboxes(mails));
 
         });
 };
 exports.getAccessibleMailsByName = function(req, res) {
-    console.log(req.body);
-    if (!req.body.params || !req.body.params.user)
-        return res.status(500).send('Empty request');
-    User
-        .findOne({
-            username: req.body.params.user
+    User.findOne({
+            username: req.body.user
         })
         .exec(function(err, user) {
             if (err) {
-                console.log(err);
                 return res.status(500).send(err);
-            } else {
-                if (user) {
-                    MailBox
-                        .find({
-                            accessedFor: user._id,
-                            state: 1,
-                            deleted: false
-                        }, {
-                            mail: 1,
-                            domain: 1
-                        }, function(err, mails) {
-                            if (err) {
-                                console.log(err);
-                                return res.status(500).send(err);
-                            } else
-                                return res.jsonp(sortMailBoxes(mails));
-                        });
-                } else
-                    return res.status(500).send('Invalid user');
+            } else if (user) {
+                mailBox
+                    .find({
+                        accessedFor: user._id,
+                        state: 1,
+                        deleted: false
+                    }, {
+                        mail: 1,
+                        domain: 1,
+
+                    }, function(err, mails) {
+                        if (err) {
+                            console.log(err);
+                            return res.status(500).send(err);
+
+                        } else return res.jsonp(sortMailboxes(mails));
+
+                    });
             }
         });
 };
-exports.provideAccessForMailBox = function(req, res) {
+exports.provideAccessForMailbox = function(req, res) {
     var users = req.body.users;
     var mails = req.body.mails;
     console.log(req.body);
     if (users) {
-        MailBox
+        mailBox
             .update({
                 _id: {
                     $in: mails
@@ -142,10 +136,10 @@ exports.provideAccessForMailBox = function(req, res) {
     }
 };
 
-exports.revokeAccessForMailBox = function(req, res) {
+exports.revokeAccessForMailbox = function(req, res) {
     var users = req.body.users;
     var mails = req.body.mails;
-    MailBox
+    mailBox
         .update({
             _id: {
                 $in: mails
@@ -167,7 +161,7 @@ exports.revokeAccessForMailBox = function(req, res) {
             }
         });
 };
-exports.synchronizeMailBoxes = function(req, res) {
+exports.synchronizemailboxes = function(req, res) {
     PackConfig
         .findOne({
             packageName: 'mailmanager'
@@ -183,10 +177,10 @@ exports.synchronizeMailBoxes = function(req, res) {
             if (data) {
                 config = data.data;
                 request(config.mailHost + (config.isPfInDefFolder ? '/postfixadmin' : config.PfCustomFolder) + '/get_mailboxes.php', function(error, response, body) {
-                    if (!error && response.statusCode === 200) {
+                     if (!error && response.statusCode === 200) {
                         var postfix = JSON.parse(body);
                         var onlyMailsFromPostfix = _.map(postfix, 'mail');
-                        MailBox
+                        mailBox
                             .find({
                                 // mail: {
                                 //     $in: onlyMailsFromPostfix
@@ -209,7 +203,7 @@ exports.synchronizeMailBoxes = function(req, res) {
                                         var notInPostfix = _.difference(onlyMailsFromMongo, onlyMailsFromPostfix);
                                         if (notInPostfix) {
                                             _.forEach(notInPostfix, function(mail, index) {
-                                                MailBox.update({
+                                                mailBox.update({
                                                     mail: mail
                                                 }, {
                                                     $set: {
@@ -233,7 +227,7 @@ exports.synchronizeMailBoxes = function(req, res) {
                                                 if (notInBase.indexOf(p.mail) !== -1)
                                                     newMails.push(p);
                                             });
-                                            MailBox.create(newMails, function(err) {
+                                            mailBox.create(newMails, function(err) {
                                                 if (err) {
                                                     console.log(err);
                                                     return res.status(500).send(err);
@@ -255,7 +249,7 @@ exports.synchronizeMailBoxes = function(req, res) {
                                                 if (result2.length > 0) {
                                                     if ((result[0].state !== result2[0].state + '') || (result[0].messages + '' !== result2[0].messages + '') || (result[0].quota + '' !== result2[0].quota + '')) {
                                                         // update element
-                                                        MailBox
+                                                        mailBox
                                                             .update({
                                                                 mail: result[0].mail
                                                             }, {
@@ -279,7 +273,7 @@ exports.synchronizeMailBoxes = function(req, res) {
                                         });
                                         return res.status(200).send();
                                     } else {
-                                        MailBox.create(postfix, function(err) {
+                                        mailBox.create(postfix, function(err) {
                                             if (err) {
                                                 console.log(err);
                                                 return res.status(500).send(err);
@@ -297,22 +291,18 @@ exports.synchronizeMailBoxes = function(req, res) {
             } else console.log('NO CONFIG TABLE');
         });
 };
-exports.getMailBoxes = function(req, res) {
-    MailBox
-        .find({
-            deleted: false
-        }, function(err, response) {
-            if (err) {
-                console.log(err);
-                return res.status(500).send(err);
-            } else {
-                return res.jsonp(sortMailBoxes(response));
-            }
-        });
+exports.getmailboxes = function(req, res) {
+    mailBox.find({
+        deleted: false
+    }, function(err, response) {
+        if (err) {
+            console.log(err);
+            return res.status(500).send(err);
+        } else {
+            return res.jsonp(sortMailboxes(response));
+        }
+    });
 };
-exports.getOneMailBox = function(req, res) {
-    User
-        .findOne({
 exports.getMailboxesNoSort = function(req, res) {
     mailBox.find({
         deleted: false
@@ -328,14 +318,14 @@ exports.getMailboxesNoSort = function(req, res) {
 exports.getOneMailbox = function(req, res) {
     User.findOne({
             _id: req.user._id,
-        })
+        }, {})
         .exec(function(err, user) {
             if (err) {
                 console.log(err);
                 return res.status(500).send(err);
             } else {
                 if (user) {
-                    MailBox
+                    mailBox
                         .findOne({
                             mail: req.body.mail,
                             deleted: false,
@@ -352,22 +342,21 @@ exports.getOneMailbox = function(req, res) {
                                         if (resault.accessedFor.indexOf(user._id) !== -1) {
                                             return res.jsonp(resault);
                                         } else {
-                                            return res.status(403).send('Access denied');
+                                            return res.status(403).send(err);
                                         }
                                     }
                                 } else {
-                                    return res.status(204).send('Empty result');
+                                    return res.status(204).send(err);
                                 }
                             }
 
                         });
                 } else {
-                    return res.status(500).send('Invalid user');
+                    return res.status(401).send(err);
                 }
             }
         });
 };
-
 exports.updateConfig = function(req, res) {
     PackConfig
         .findOne({
--- packages/mailmanager/server/routes/mailmanager.js
+++ packages/mailmanager/server/routes/mailmanager.js
@@ -4,19 +4,17 @@ module.exports = function(Mailmanager, app, auth, database) {
 
     var mailmanager = require('../controllers/mailmanager.js');
 
-    app.get('/api/synchronizemailboxes', auth.requiresAdmin, mailmanager.synchronizeMailBoxes);
-    app.get('/api/getAllMailboxes', auth.requiresAdmin, mailmanager.getMailBoxes);
+
     app.get('/api/synchronizemailboxes', auth.requiresAdmin, mailmanager.synchronizemailboxes);
     app.get('/api/getAllMailboxes', auth.requiresAdmin, mailmanager.getmailboxes);
     app.get('/api/getAllMailboxesNoSort', auth.requiresAdmin, mailmanager.getMailboxesNoSort);
     app.get('/api/getConfig', auth.requiresAdmin, mailmanager.getConfig);
     app.post('/api/updateConfig', auth.requiresAdmin, mailmanager.updateConfig);
-    app.post('/api/provideAccessForMailbox', auth.requiresAdmin, mailmanager.provideAccessForMailBox);
-    app.post('/api/revokeAccessForMailbox', auth.requiresAdmin, mailmanager.revokeAccessForMailBox);
+    app.post('/api/provideAccessForMailbox', auth.requiresAdmin, mailmanager.provideAccessForMailbox);
+    app.post('/api/revokeAccessForMailbox', auth.requiresAdmin, mailmanager.revokeAccessForMailbox);
     app.get ('/api/getAccessibleMails', auth.requiresLogin, mailmanager.getAccessibleMails);
     app.post ('/api/getAccessibleMailsByName', auth.requiresLogin, mailmanager.getAccessibleMailsByName);  
     app.get ('/api/getMailConfig', auth.requiresLogin, mailmanager.getConfig);
-    app.post ('/api/getOneMailbox', auth.requiresLogin, mailmanager.getOneMailBox);
-    app.get ('/api/searchMailboxes', auth.requiresLogin, mailmanager.searchMailboxes);
     app.post ('/api/getOneMailbox', auth.requiresLogin, mailmanager.getOneMailbox);
+    app.get ('/api/searchMailboxes', auth.requiresLogin, mailmanager.searchMailboxes);
 };
