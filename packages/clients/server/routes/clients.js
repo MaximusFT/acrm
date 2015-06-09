aaeea59deee4915c2434dfa659d822e32922b319
Nikita Malik <nick.v.malik@gmail.com>
2015-06-09 18:01:04 +0300

    Grant access for requests to Kostya

--- packages/clients/server/routes/clients.js
+++ packages/clients/server/routes/clients.js
@@ -7,15 +7,15 @@ module.exports = function(Clients, app, auth, database) {
         requests = require('../controllers/requests.js');
 
     app.post('/webform/crm/web_request_form_add', webreqs.web_request_form_add);
-    app.get('/api/webreqs', auth.requiresAdmin, webreqs.webreqs);
-    app.get('/api/oldWebreqs', auth.requiresAdmin, webreqs.oldWebreqs);
+    app.get('/api/webreqs', auth.requiresAdminAndKostya, webreqs.webreqs);
+    app.get('/api/oldWebreqs', auth.requiresAdminAndKostya, webreqs.oldWebreqs);
     app.get('/api/phonesForWebinars', auth.requiresAdmin, webreqs.phonesForWebinars);
     app.get('/api/acrmRequestTypes', auth.requiresLogin, webreqs.acrmRequestTypes);
-    app.post('/api/applyFilters', auth.requiresAdmin, webreqs.applyFilters);
-    app.put('/api/changeWebreqState/:webreqId', auth.requiresAdmin, webreqs.changeWebreqState);
-    app.get('/api/webreqs/reports', auth.requiresAdmin, webreqs.reports);
-    app.get('/api/webrequest/:webreqId', auth.requiresAdmin, webreqs.webreq);
-    app.get('/api/reportForWebreq/:webreqId', auth.requiresAdmin, webreqs.reportForWebreq);
+    app.post('/api/applyFilters', auth.requiresAdminAndKostya, webreqs.applyFilters);
+    app.put('/api/changeWebreqState/:webreqId', auth.requiresAdminAndKostya, webreqs.changeWebreqState);
+    app.get('/api/webreqs/reports', auth.requiresAdminAndKostya, webreqs.reports);
+    app.get('/api/webrequest/:webreqId', auth.requiresAdminAndKostya, webreqs.webreq);
+    app.get('/api/reportForWebreq/:webreqId', auth.requiresAdminAndKostya, webreqs.reportForWebreq);
 
     app.get('/api/getDocumentFields', requests.getDocumentFields);
     app.post('/api/sendUserRequest', requests.processUserRequest);
